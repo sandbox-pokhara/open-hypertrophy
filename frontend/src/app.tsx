711e45417bcdb68f4938c96b84aabc5ff5739358
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2024-12-01 15:45:52 +0545

    refactor: rename modules

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Login from "./pages/login/login";
-import SignUp from "./pages/signup/sign-up";
+import SignUp from "./pages/sign-up/sign-up";
 import { SidebarProvider } from "./components/ui/sidebar";
 
 const queryClient = new QueryClient({
